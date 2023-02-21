4f0ea05d443e82d25d5378fa02ac41bf204a5d55
柳崑德 <asf123456130@gmail.com>
2023-02-21 13:56:27 +0800

    加入專案檔案。

---
+++ 奇偶判斷/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 奇偶判斷
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一串以逗號分隔的整數數字字串");
+            string[] input = Console.ReadLine().Split(',');
+
+            var odd = input.Where(x => int.Parse(x) % 2 == 1).OrderBy(x => x);
+            var even = input.Where(x => int.Parse(x) % 2 == 0).OrderBy(x => x);
+
+            Console.WriteLine("奇數 : " + string.Join(",", odd));
+            Console.WriteLine("偶數 : " + string.Join(",", even));
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 序列倒置/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 序列倒置
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一串以逗號分隔的字串");
+            string input = Console.ReadLine();
+            List<string> list = input.Split(',').ToList();
+
+            list.Reverse();
+            
+            Console.WriteLine(string.Join(",", list));
+
+            
+            
+        }
+    }
+}
